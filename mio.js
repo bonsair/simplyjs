b310e6db563b5327fb88c50f0f932571c6ffe24f
bonsair <rcascal@hotmail.com>
2014-04-11 11:12:48 +0200

    Update mio.js

--- mio.js
+++ mio.js
@@ -1,17 +1,1 @@
-console.log('Simply.js demo!');
-
-simply.on('singleClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Pressed ' + e.button + '!');
-});
-
-simply.on('longClick', function(e) {
-  console.log(util2.format('long clicked $button!', e));
-  simply.vibe();
-  simply.scrollable(e.button !== 'select');
-});
-
-simply.setText({
-  title: 'Simply Demo!',
-  body: 'This is a demo. Press buttons or tap the watch!',
-}, true);
+console.log('Lo basico');
